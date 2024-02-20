a30ec97642416ef3ab89bf103397183c920932b2
Sisphus-Edge <2375411682@qq.com>
2024-02-20 12:44:49 +0800

    test

--- lib/routes/first_level_page/pet_home.dart
+++ lib/routes/first_level_page/pet_home.dart
@@ -75,7 +75,6 @@ class _PetHomeRouteState extends State<PetHomeRoute> {
                 ),
 
 
-
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(15), // 圆角半径
                   child: Image.asset(
